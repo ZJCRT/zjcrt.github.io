818128e7a8e74e5c25de75993c98227f4c2ca980
ZJCRT <steffen.urban@zeiss.com>
2020-11-03 11:26:55 +0100

    fix

--- aruco_detector.js
+++ aruco_detector.js
@@ -7,7 +7,6 @@ let streaming = false;
 
 // Some HTML elements we need to configure.
 let video = null;
-let checkbox = null;
 let start = null;
 let stop = null;
 let stream = null;
@@ -44,14 +43,12 @@ function initVideo(ev){
         video.setAttribute("height", height);
         streaming = true;
     }
-    checkbox.disabled = false;
     stop.disabled = false;
     playVideo();
 }
 
 function startup() {
     video = document.getElementById("video");
-    checkbox = document.getElementById("checkbox");
     start = document.getElementById("startup");
     stop = document.getElementById("stop");
     // start camera
@@ -66,6 +63,7 @@ function startup() {
     });
 
     video.addEventListener("canplay", initVideo, false);
+    main()
 }
 
 function playVideo() {
@@ -147,62 +145,64 @@ if (typeof cv !== 'undefined') {
     document.getElementById("opencvjs").onload = onReady;
 }
 
-// inputImage are declared and deleted elsewhere
-inputImage = new cv.Mat(height, width, cv.CV_8UC4);
-markerImage = new cv.Mat();
-//dictionary = new cv.Dictionary(0);
-//dictionary.markerSize = 3;
-//dictionary.maxCorrectionBits = 1;
-//dictionary.bytesList.delete();
-//// dictionary.bytesList = cv.matFromArray(1, 2, cv.CV_8UC4, [197, 71,  81, 248, 226, 163, 31, 138]);
-//dictionary.bytesList = cv.matFromArray(1, 2, cv.CV_8UC4, [177, 0, 135, 0, 70, 1, 112, 1]);
-dictionary = new cv.Dictionary(cv.DICT_ARUCO_ORIGINAL);
-parameter = new cv.DetectorParameters();
 
-// parameter.adaptiveThreshWinSizeMin = 3,
-parameter.adaptiveThreshWinSizeMin = 23;
-// parameter.adaptiveThreshWinSizeMax = 23,
-parameter.adaptiveThreshWinSizeMax = 23;
-parameter.adaptiveThreshWinSizeStep = 10,
-parameter.adaptiveThreshConstant = 7;
-// parameter.minMarkerPerimeterRate = 0.03;
-parameter.minMarkerPerimeterRate = 0.1;
-parameter.maxMarkerPerimeterRate = 4;
-parameter.polygonalApproxAccuracyRate = 0.03;
-parameter.minCornerDistanceRate = 0.05;
-parameter.minDistanceToBorder = 3;
-parameter.minMarkerDistanceRate = 0.05;
-parameter.cornerRefinementMethod = cv.CORNER_REFINE_SUBPIX; // CORNER_REFINE_NONE
-parameter.cornerRefinementWinSize = 5;
-parameter.cornerRefinementMaxIterations = 10;
-parameter.cornerRefinementMinAccuracy = 0.1;
-parameter.markerBorderBits = 1;
-// parameter.perspectiveRemovePixelPerCell = 4;
-parameter.perspectiveRemovePixelPerCell = 2;
-parameter.perspectiveRemoveIgnoredMarginPerCell = 0.13;
-parameter.maxErroneousBitsInBorderRate = 0.35;
-parameter.minOtsuStdDev = 5.0;
-parameter.errorCorrectionRate = 0.6;
 
-markerIds = new cv.Mat();
-markerCorners  = new cv.MatVector();
-rvecs = new cv.Mat();
-tvecs = new cv.Mat();
-RgbImage = new cv.Mat();
-let fx = 1000;
-let fy = 1000;
-let cx = 1920.0 / 2.0;
-let cy = 1080.0 / 2.0;
+function main() {
+    // inputImage are declared and deleted elsewhere
+    inputImage = new cv.Mat(height, width, cv.CV_8UC4);
+    markerImage = new cv.Mat();
+    //dictionary = new cv.Dictionary(0);
+    //dictionary.markerSize = 3;
+    //dictionary.maxCorrectionBits = 1;
+    //dictionary.bytesList.delete();
+    //// dictionary.bytesList = cv.matFromArray(1, 2, cv.CV_8UC4, [197, 71,  81, 248, 226, 163, 31, 138]);
+    //dictionary.bytesList = cv.matFromArray(1, 2, cv.CV_8UC4, [177, 0, 135, 0, 70, 1, 112, 1]);
+    dictionary = new cv.Dictionary(cv.DICT_ARUCO_ORIGINAL);
+    parameter = new cv.DetectorParameters();
 
-cameraMatrix = cv.matFromArray(3, 3, cv.CV_64F, 
-                                [9.6635571716090658e+02, 0., 2.0679307818305685e+02, 0.,
-                                 9.6635571716090658e+02, 2.9370020600555273e+02, 0., 0., 1.]);
-distCoeffs = cv.matFromArray(5, 1, cv.CV_64F, [0.0,0.0,0.0,0.0,0.0]);
-// "video" is the id of the video tag
+    // parameter.adaptiveThreshWinSizeMin = 3,
+    parameter.adaptiveThreshWinSizeMin = 23;
+    // parameter.adaptiveThreshWinSizeMax = 23,
+    parameter.adaptiveThreshWinSizeMax = 23;
+    parameter.adaptiveThreshWinSizeStep = 10,
+    parameter.adaptiveThreshConstant = 7;
+    // parameter.minMarkerPerimeterRate = 0.03;
+    parameter.minMarkerPerimeterRate = 0.1;
+    parameter.maxMarkerPerimeterRate = 4;
+    parameter.polygonalApproxAccuracyRate = 0.03;
+    parameter.minCornerDistanceRate = 0.05;
+    parameter.minDistanceToBorder = 3;
+    parameter.minMarkerDistanceRate = 0.05;
+    parameter.cornerRefinementMethod = cv.CORNER_REFINE_SUBPIX; // CORNER_REFINE_NONE
+    parameter.cornerRefinementWinSize = 5;
+    parameter.cornerRefinementMaxIterations = 10;
+    parameter.cornerRefinementMinAccuracy = 0.1;
+    parameter.markerBorderBits = 1;
+    // parameter.perspectiveRemovePixelPerCell = 4;
+    parameter.perspectiveRemovePixelPerCell = 2;
+    parameter.perspectiveRemoveIgnoredMarginPerCell = 0.13;
+    parameter.maxErroneousBitsInBorderRate = 0.35;
+    parameter.minOtsuStdDev = 5.0;
+    parameter.errorCorrectionRate = 0.6;
 
-loopIndex = setInterval(
-    function(){
-        if (checkbox.checked) {
+    markerIds = new cv.Mat();
+    markerCorners  = new cv.MatVector();
+    rvecs = new cv.Mat();
+    tvecs = new cv.Mat();
+    RgbImage = new cv.Mat();
+    let fx = 1.2*width;
+    let fy = 1.2*width;
+    let cx = width / 2.0;
+    let cy = height / 2.0;
+
+    cameraMatrix = cv.matFromArray(3, 3, cv.CV_64F, 
+                                    [fx, 0., cx, 
+                                     0., fy, cy, 
+                                     0., 0., 1.]);
+    distCoeffs = cv.matFromArray(5, 1, cv.CV_64F, [0.0,0.0,0.0,0.0,0.0]);
+    // "video" is the id of the video tag
+    loopIndex = setInterval(
+        function(){
             // disable video showing on left side
             document.getElementById("video").style.display="none";
 
@@ -229,7 +229,5 @@ loopIndex = setInterval(
             document.getElementById("nrdetectedmarkers").innerHTML = markerIds.rows;
 
             cv.imshow("canvasOutput", RgbImage);
-        }
-        else
-            cv.imshow("canvasOutput", inputImage);
-    }, 33);
+        }, 33);
+}
