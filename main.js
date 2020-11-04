c3a4ac1745489e97df0ee2a28c0f761ec4dc321b
Steffen Urban <urbste@googlemail.com>
2020-11-04 15:22:07 +0100

    AR is working

--- main.js
+++ main.js
@@ -31,6 +31,35 @@ let board = null;
 
 let run_interval = 30; // fps
 
+
+// setup a basic scene
+let scene = new THREE.Scene();
+let renderer = new THREE.WebGLRenderer({ alpha: true } ); 
+renderer.shadowMap.enabled = true;
+let render_camera = null;
+//// This is where we create our off-screen render target ////
+const geometry = new THREE.BoxGeometry(0.08,0.08,0.08);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add(cube);
+ 
+
+
+// // Create a different scene to hold our buffer objects
+// let bufferScene = new THREE.Scene();
+// // Create the texture that will store our result
+// let bufferTexture = null;
+ 
+ 
+function render() {
+    requestAnimationFrame( render );
+    // Render onto our off-screen texture
+    // renderer.render(bufferScene, camera, bufferTexture);
+    // Finally, draw to the screen
+    renderer.render( scene, camera );
+}
+
+
 function read(a) {
     alert(a);
 }
@@ -84,12 +113,93 @@ function startup() {
     video.addEventListener("canplay", initVideo, false);
 }
 
+function NewMatrix3(
+	m11, m12, m13,
+	m21, m22, m23,
+	m31, m32, m33
+) {
+	var matrix = new THREE.Matrix3();
+	matrix.set(
+		m11, m12, m13,
+		m21, m22, m23,
+		m31, m32, m33);
+	return matrix;
+}
+
+
+function CreateOpenCv2WebGlCorrectionMatrix() {
+	var openCv2WebGl = new THREE.Matrix3();
+	openCv2WebGl.multiplyMatrices(
+		NewMatrix3(
+			Math.cos(Math.PI), -Math.sin(Math.PI), 0,
+			Math.sin(Math.PI), Math.cos(Math.PI), 0,
+			0, 0, 1), 
+		NewMatrix3(
+			Math.cos(Math.PI), 0, -Math.sin(Math.PI),
+			0, 1, 0,
+			Math.sin(Math.PI), 0, Math.cos(Math.PI)));
+	return openCv2WebGl;
+}
+
+function TransformToWebGlPose(pose, correctionMatrix) {
+	var position = MathUtils.GetPosition(pose.matrix);
+	var correctedOrientation = ApplyCorrection(correctionMatrix, MathUtils.GetRotationMatrix(pose.matrix)).elements;
+	var converted = new THREE.Matrix4();
+	converted.set(
+		correctedOrientation[0], correctedOrientation[3], correctedOrientation[6], position.x,
+		correctedOrientation[1], correctedOrientation[4], correctedOrientation[7], position.y,
+		correctedOrientation[2], correctedOrientation[5], correctedOrientation[8], position.z,
+		0, 0, 0, 1);
+	pose.matrix = converted;
+	return pose;
+}
+
 function aruco() {
     // inputImage are declared and deleted elsewhere
     markerImage = new cv.Mat();
     dictionary = new cv.aruco_Dictionary(cv.DICT_ARUCO_ORIGINAL);
     //parameter = new cv.DetectorParameters();
     board = new cv.aruco_GridBoard(2,2, 0.08, 0.02, dictionary);
+
+    // let objPts =  [[0.        , 0.17999999, 0.        ],
+    //                [0.08      , 0.17999999, 0.        ],
+    //                [0.08      , 0.09999999, 0.        ],
+    //                [0.        , 0.09999999, 0.        ],
+    //                [0.09999999, 0.17999999, 0.        ],
+    //                [0.17999999, 0.17999999, 0.        ],
+    //                [0.17999999, 0.09999999, 0.        ],
+    //                [0.09999999, 0.09999999, 0.        ],
+    //                [0.  , 0.08, 0.  ],
+    //                [0.08, 0.08, 0.  ],
+    //                [0.08, 0.  , 0.  ],
+    //                [0.  , 0.  , 0.  ],
+    //                [0.09999999, 0.08      , 0.        ],
+    //                [0.17999999, 0.08      , 0.        ],
+    //                [0.17999999, 0.        , 0.        ],
+    //                [0.09999999, 0.        , 0.        ]]
+
+    let objPts = {
+                "100" : 
+                  [0.        , 0.17999999, 0.        ,
+                   0.08      , 0.17999999, 0.        ,
+                   0.08      , 0.09999999, 0.        ,
+                   0.        , 0.09999999, 0.        ],
+                "101" : 
+                  [0.09999999, 0.17999999, 0.        ,
+                  0.17999999, 0.17999999, 0.        ,
+                  0.17999999, 0.09999999, 0.        ,
+                  0.09999999, 0.09999999, 0.        ],
+                "102" : 
+                  [0.  , 0.08, 0.  ,
+                   0.08, 0.08, 0.  ,
+                   0.08, 0.  , 0.  ,
+                   0.  , 0.  , 0.  ],
+                "103" : 
+                  [0.09999999, 0.08      , 0.        ,
+                   0.17999999, 0.08      , 0.        ,
+                   0.17999999, 0.        , 0.        ,
+                   0.09999999, 0.        , 0.        ]
+            };
 
     // // parameter.adaptiveThreshWinSizeMin = 3,
     // parameter.adaptiveThreshWinSizeMin = 23;
@@ -144,22 +254,83 @@ function aruco() {
 
             cv.cvtColor(inputImage, RgbImage, cv.COLOR_RGBA2RGB, 0);
             cv.detectMarkers(RgbImage, dictionary, markerCorners, markerIds);
+            let nrDetectedPts = 4*markerCorners.size();
+            
 
             let endTime = performance.now();    
-
             if (markerIds.rows > 0) {
+                let cornersJs = [];
+                let objPtsJs = [];
+                for (let i = 0; i < markerCorners.size(); ++i) {
+                    let corners = markerCorners.get(i);
+                    let id = markerIds.intAt(0,i);
+                    for (let c = 0; c < 8; ++c) {
+                        cornersJs.push(corners.floatAt(0,c));
+                    }
+                    for (let c = 0; c < 3*4; ++c) {
+                        objPtsJs.push(objPts[id][c]) 
+                     }
+                }
+    
+                let objPointsCV = cv.matFromArray(nrDetectedPts, 3, cv.CV_64F, objPtsJs)
+                let cornerPtsCV = cv.matFromArray(nrDetectedPts, 2, cv.CV_64F, cornersJs)
 
+                let rvec = new cv.Mat();
+                let tvec = new cv.Mat();
                 cv.drawDetectedMarkers(RgbImage, markerCorners, markerIds);
+                let valid = cv.solvePnP(objPointsCV, cornerPtsCV, cameraMatrix, distCoeffs, rvec, tvec, false, cv.SOLVEPNP_IPPE)
                 // valid = cv.estimatePoseBoard(markerCorners, markerIds, board, cameraMatrix, distCoeffs, rvec, tvec);
-                // let rotationMat = new cv.Mat();
-                // if (valid) {
+                let R_cv = new cv.Mat();
+                if (valid) {
+                
+                    cv.Rodrigues(rvec, R_cv);
+                    cv.drawAxis(RgbImage, cameraMatrix, distCoeffs, rvec, tvec, 0.1);
+
+                    R_cv_ = new math.matrix([
+                        [R_cv.doubleAt(0,0),R_cv.doubleAt(0,1),R_cv.doubleAt(0,2)],
+                        [R_cv.doubleAt(1,0),R_cv.doubleAt(1,1),R_cv.doubleAt(1,2)],
+                        [R_cv.doubleAt(2,0),R_cv.doubleAt(2,1),R_cv.doubleAt(2,2)]]);
+                    t_cv_ = new math.matrix([tvec.doubleAt(0,0), tvec.doubleAt(0,1), tvec.doubleAt(0,2)])
+                    R_cv_t = math.transpose(R_cv_);
+                    minus_ = math.matrix([[-1,-1,-1],
+                        [-1,-1,-1],
+                        [-1,-1,-1]])
+                    const X = math.multiply(math.dotMultiply(minus_,R_cv_t), t_cv_)
+
+                    ocv2ogl = new math.matrix([
+                        [1,0,0],
+                        [0,-1,0],
+                        [0,0,-1]
+                    ])
+                    R_cv_t_corrected = math.multiply(R_cv_t, ocv2ogl)
+                    //let ocv2ogl = cv.matFromArray(3,3, cv.CV_64F, [1,0,0,0,-1,0,0,0,-1]);
+                    //let threejs_X = new cv.Mat();
+                    //threejs_X = R_cv.t() * tvec;
+
                     
-                //     cv.Rodrigues(rvec, rotationMat);
-                //     cv.drawAxis(RgbImage, cameraMatrix, distCoeffs, rvec, tvec, 0.1);
-                // }
-                // rotationMat.delete();
-                // rvec.delete();
-                // tvec.delete();
+                    var quaternion = new THREE.Quaternion();
+                    var matrix = new THREE.Matrix4();
+                    var correctedOrientation = new THREE.Matrix3();
+
+                    matrix.set(
+                        R_cv_t_corrected._data[0][0], R_cv_t_corrected._data[0][1], R_cv_t_corrected._data[0][2], X._data[0],
+                        R_cv_t_corrected._data[1][0], R_cv_t_corrected._data[1][1], R_cv_t_corrected._data[1][2], X._data[1],
+                        R_cv_t_corrected._data[2][0], R_cv_t_corrected._data[2][1], R_cv_t_corrected._data[2][2], X._data[2],
+                        0, 0, 0, 1);
+                        
+                    quaternion.setFromRotationMatrix(matrix)
+                    render_camera.position.set(X._data[0], X._data[1], X._data[2]);        
+                    render_camera.quaternion.copy(quaternion);
+                    //render_camera.lookAt(0,0,0);
+                    
+                    console.log(tvec.doubleAt(0,2))
+                    console.log(render_camera.quaternion)
+                    console.log(render_camera.position)
+                    render();
+                }
+                R_cv.delete();
+                rvec.delete();
+                tvec.delete();
             }
             
             var timeDiff = endTime - startTime; //in ms 
@@ -167,6 +338,7 @@ function aruco() {
             document.getElementById("nrdetectedmarkers").innerHTML = markerIds.rows;
 
             cv.imshow("canvasOutput", RgbImage);
+            
         }, run_interval);
 
 }
@@ -202,7 +374,17 @@ function laplacian() {
 
 }
 
- function  playVideo() {
+function render() {
+    renderer.setClearColor(0x000000, 0);
+    
+    // Render onto our off-screen texture
+    //renderer.render(bufferScene, render_camera, bufferTexture);
+    // Finally, draw to the screen
+    renderer.render(scene, render_camera );
+    requestAnimationFrame( render );
+}
+
+function  playVideo() {
     if (!streaming) {
         console.warn("Please startup your webcam");
         return;
@@ -210,6 +392,10 @@ function laplacian() {
 
     inputImage = new cv.Mat(height, width, cv.CV_8UC4);
 
+    bufferTexture = new THREE.WebGLRenderTarget(width, height, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
+    renderer.setSize(width, height);
+    document.body.appendChild( renderer.domElement );
+    render_camera = new THREE.PerspectiveCamera( 100, width/height, 0.01, 10 );
 
     start.disabled = true;
     if (document.getElementById("aruco_test_content")) {
