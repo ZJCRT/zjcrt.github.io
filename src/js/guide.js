e7add8f97e45a7e34e1b8a20bc8ae50543c38af1
Marcel Walch <marcel.walch@zeiss.com>
2020-11-30 12:17:14 +0100

    video autostart

--- src/js/guide.js
+++ src/js/guide.js
@@ -1,8 +1,25 @@
 //'use strict';
 import cv_service from '../services/cv_service.js';
 
+// Video dimensions
+let width = 0;
+let height = 0;
+
+// whether streaming video from the camera.
+let streaming = false;
+
 // Some HTML elements we need to configure.
 let video = null;
+let stream = null;
+
+var constraints = {
+    audio: false,
+    video: {
+        facingMode: "environment",
+        width: { min: 320, max: 640 },
+        height: { min: 240, max: 480 },
+    },
+};
 
 function startup() {
     video = document.getElementById("video");
@@ -29,11 +46,15 @@ function initVideo(ev){
         video.setAttribute("width", width);
         video.setAttribute("height", height);
 
-        videoImageContext.clearRect(0, 0, width, height);
-
         streaming = true;
-        stop.disabled = false;
         playVideo();
+    }
+}
+
+function playVideo() {
+    if (!streaming) {
+        console.warn("Please startup your webcam");
+        return;
     }
 }
 
