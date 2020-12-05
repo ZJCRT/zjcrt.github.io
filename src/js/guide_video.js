b95ee5a217c3279ae984bdb395406ca1278c3c05
Marcel Walch <marcel.walch@zeiss.com>
2020-12-05 12:24:59 +0100

    trying to get calib running again

--- src/js/guide_video.js
+++ src/js/guide_video.js
@@ -1,4 +1,6 @@
 import * as THREE from '../js_libs/three.module.js';
+import { GUI } from '../js_libs/dat.gui.module.js';
+import cv_service from '../services/cv_service.js';
 
 
 let video, videoTexture, videoMesh;
@@ -13,6 +15,42 @@ var mediaConstraints = {
 		facingMode: "environment"
     }
 };
+
+const debugGUI = new GUI();
+
+let debugOptions = {
+	openCV_ready: false,
+	cameraCalibration: {
+		use_new_board_checker: false,
+		takeCalibImage: function() {
+			estimatePoseAruco();
+		}
+	},
+	reset: function() {
+		console.log("reset");
+	  	this.use_new_board_checker = false;
+	//   camera.position.z = 75;
+	//   camera.position.x = 0;
+	//   camera.position.y = 0;
+	//   cube.scale.x = 1;
+	//   cube.scale.y = 1;
+	//   cube.scale.z = 1;
+	//   cube.material.wireframe = true;
+	}
+};
+
+function initDebugGUI(){
+	//https://codepen.io/programking/pen/MyOQpO
+
+	debugGUI.add(debugOptions, 'openCV_ready').listen();
+	
+	var calib = debugGUI.addFolder('Camera Calibration');
+	calib.add(debugOptions.cameraCalibration, 'use_new_board_checker').listen();
+	calib.add(debugOptions.cameraCalibration, 'takeCalibImage').listen();
+	calib.open();
+
+	debugGUI.add(debugOptions, 'reset');
+}
 
 function init() {
 	camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight,0.1,100);
@@ -53,15 +91,30 @@ function init() {
 	} else {
 		console.error('MediaDevices interface not available.');
 	}
+
+	// load webworker
+	cv_service.loadArucoWebWorker();
+
+	//wait until openCV is loaded ... then display dialog
+    cv_service.worker.addEventListener('message', function(e){
+        if(e.data.msg=="load"){
+			debugOptions.openCV_ready = true;
+            displayDialog("Calibrate Camera", "Take some images of the markerboard", "Start calibration");
+        }
+    },false);
 }
+
+
 
 function updateCameraChange(){
 	camera.updateProjectionMatrix();
-	
+
 	//TODO check for other aspect ratio?
 	videoMesh.scale.y = Math.tan(camera.fov * Math.PI / 180 * 0.5) * vidDistToCam * 2 ;
 	videoMesh.scale.x = videoMesh.scale.y;
 }
+
+
 
 
 function render(){
@@ -71,3 +124,48 @@ function render(){
 
 init();
 render();
+initDebugGUI();
+
+
+
+
+//###### Calibration
+
+let canvas = document.createElement('canvas');
+let ctx = canvas.getContext('2d');
+
+let camera_matrix, dist_coeffs;
+
+// send image data to the webworker
+async function estimatePoseAruco() {
+	
+    ctx.drawImage(videoTexture.image, 0, 0);
+	const image_data = ctx.getImageData(0,0,1080,1920);
+	
+	const pose_payload = await cv_service.poseEstimation(
+        {"image" : image_data, "camera_matrix" : camera_matrix, "dist_coeffs" : dist_coeffs, "use_new_board": debugOptions.cameraCalibration.use_new_board_checker});
+    const pose = pose_payload.data.payload;
+    const quat_xyzw = pose["quaternion_xyzw"];
+    const quaternion = new THREE.Quaternion().set(quat_xyzw[0],quat_xyzw[1],quat_xyzw[2],quat_xyzw[3]).normalize();
+
+    if (pose["valid"]) {
+        document.getElementById("cam_pose_status").innerHTML = "Cam pose VALID. Pose (xyz, qxqyqzqw): "+
+            pose["position"][0].toFixed(3) + ", " + pose["position"][1].toFixed(3)+ ", " + pose["position"][2].toFixed(3) + ", " + 
+            quat_xyzw[0].toFixed(3) + ", " + quat_xyzw[1].toFixed(3)+ ", " + quat_xyzw[2].toFixed(3) +", " + quat_xyzw[3].toFixed(3);
+        document.getElementById("cam_pose_status").style.color = "green";
+    } else {
+        document.getElementById("cam_pose_status").innerHTML = "Cam pose INVALID. Pose (xyz, qxqyqzqw): "+
+            pose["position"][0].toFixed(3) + ", " + pose["position"][1].toFixed(3)+ ", " + pose["position"][2].toFixed(3) + ", " +
+            quat_xyzw[0].toFixed(3) + ", " + quat_xyzw[1].toFixed(3)+ ", " + quat_xyzw[2].toFixed(3) +", " + quat_xyzw[3].toFixed(3);
+        document.getElementById("cam_pose_status").style.color = "red";
+    }
+    document.getElementById("trackingtime").innerHTML = "Tracking time: "+pose["est_time"].toFixed(2);
+    if (pose["est_time"] > 30) {
+        document.getElementById("trackingtime").style.color = "red";
+    } else {
+        document.getElementById("trackingtime").style.color = "green";
+    }
+    render_camera.position.set(pose["position"][0], pose["position"][1], pose["position"][2]);        
+    render_camera.quaternion.copy(quaternion);
+
+}
