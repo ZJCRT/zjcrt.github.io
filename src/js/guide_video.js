2161d63d738cd1ac629586d78175fada976642d5
Marcel Walch <marcel.walch@zeiss.com>
2020-12-17 10:17:18 +0100

    more cleanup

--- src/js/guide_video.js
+++ src/js/guide_video.js
@@ -1,7 +1,7 @@
 import * as THREE from '../js_libs/three.module.js';
 import cv_service from '../services/cv_service.js';
 
-export{cv_service, options, scene, render_camera, estimateCameraIntrinsics, renderer, setDistToMeasure};
+export { cv_service, options, scene, render_camera, estimateCameraIntrinsics, renderer, setDistToMeasure };
 
 let video, videoTexture, videoMesh;
 let renderer, scene, render_camera, rendercanvas;
@@ -21,12 +21,12 @@ let poseEstimationRunning = false; // is the webworker currently bussy
 const poseTimes = []; // stores timestamps to calculate FPS of pose estimation
 
 var mediaConstraints = {
-    audio: false,
-    video: {
+	audio: false,
+	video: {
 		width: 1920,
-    	height: 1080,
+		height: 1080,
 		facingMode: "environment"
-    }
+	}
 };
 
 let options = {
@@ -55,47 +55,42 @@ let options = {
 };
 
 // To get the scale from the markerboard right, the user has to measure the width of the markerboard (red line)
-function setDistToMeasure(dist){
+function setDistToMeasure(dist) {
 	dist_to_measure = dist;
 }
 
 function init() {
 	//Camera will be updated as soon as it is calibrated -> updateCameraChange()
-	render_camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight,0.01,100);
+	render_camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 100);
 	render_camera.position.z = 0.5;
-	
+
 	scene = new THREE.Scene();
 
-	video =  document.getElementById("video");
+	video = document.getElementById("video");
 	videoTexture = new THREE.VideoTexture(video);
-	video.addEventListener( "loadedmetadata", function (e) {
-		console.log( "VideoTextureResolution: "+videoTexture.image.videoWidth+"x"+videoTexture.image.videoHeight );  
-	}, false );
 
-	const geometry = new THREE.PlaneBufferGeometry(options.dimensions.planebuffer.width,options.dimensions.planebuffer.height);
-	const material = new THREE.MeshBasicMaterial({map: videoTexture});
-	videoMesh = new THREE.Mesh(geometry,material);
+	video.addEventListener("loadedmetadata", function (e) {
+		console.log("VideoTextureResolution: " + videoTexture.image.videoWidth + "x" + videoTexture.image.videoHeight);
+	}, false);
+
+	const geometry = new THREE.PlaneBufferGeometry(options.dimensions.planebuffer.width, options.dimensions.planebuffer.height);
+	const material = new THREE.MeshBasicMaterial({ map: videoTexture });
+	videoMesh = new THREE.Mesh(geometry, material);
 	render_camera.add(videoMesh);
 	scene.add(render_camera);
-	
-	rendercanvas = document.getElementById("rendercanvas");
-	renderer = new THREE.WebGLRenderer({canvas: rendercanvas, antialias: true});
-	renderer.setSize(window.innerWidth, window.innerHeight);
 
-	console.log("init...");
-	console.log(`init camera settings to fov: ${render_camera.fov}, aspect:  ${render_camera.aspect}`);
-	console.log(render_camera);
-	console.log(renderer);
-	console.log(videoMesh);
+	rendercanvas = document.getElementById("rendercanvas");
+	renderer = new THREE.WebGLRenderer({ canvas: rendercanvas, antialias: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
 	updateCameraChange();
 
 	//Get Camerastream
-	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
-		navigator.mediaDevices.getUserMedia(mediaConstraints).then(function(stream){
+	if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices.getUserMedia(mediaConstraints).then(function (stream) {
 			video.srcObject = stream;
 			video.play();
-		}).catch(function(error){
+		}).catch(function (error) {
 			console.error('Unable to access the cam.', error);
 		});
 	} else {
@@ -109,49 +104,49 @@ function init() {
 	addOpenCVLoadListener(openCVLoaded);
 }
 
-export {addOpenCVLoadListener};
-function addOpenCVLoadListener(callback){
-	cv_service.worker.addEventListener('message', function(e){
-        if(e.data.msg=="load"){
+export { addOpenCVLoadListener };
+function addOpenCVLoadListener(callback) {
+	cv_service.worker.addEventListener('message', function (e) {
+		if (e.data.msg == "load") {
 			callback();
 		}
-    },false);
+	}, false);
 }
 
-function openCVLoaded(){
+function openCVLoaded() {
 	options.openCV_ready = true;
 }
 
 function stopCamera() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);    
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	video.pause();
 	video.srcObject.getVideoTracks()[0].stop();
-    video.srcObject = null;
+	video.srcObject = null;
 }
 
 
 
-function updateCameraChange(){
+function updateCameraChange() {
 	render_camera.updateProjectionMatrix();
 	let vFOV = THREE.MathUtils.degToRad(render_camera.fov);
 	// let hFOV = vFOV * render_camera.aspect;
 
-	videoMesh.position.z = -1 * options.dimensions.planebuffer.height / (2 * Math.tan (vFOV/2));
+	videoMesh.position.z = -1 * options.dimensions.planebuffer.height / (2 * Math.tan(vFOV / 2));
 
 	// TODO fill the screen on all devices that have a different aspect ratio than 9:16 
 	// render_camera.zoom = 1.4; // works on iPad Pro
 	// console.log(`update camera change fov: ${render_camera.fov}, aspect:  ${render_camera.aspect}, videoMesh.position.z: ${videoMesh.position.z}, zoom: ${render_camera.zoom},  focalLength: ${render_camera.getFocalLength()}`);
 }
 
-function render(){
+function render() {
 	requestAnimationFrame(render);
 
-	if(camera_initialized && ! poseEstimationRunning){
+	if (camera_initialized && !poseEstimationRunning) {
 		estimatePoseAruco();
 	}
 
-	renderer.render(scene,render_camera);
+	renderer.render(scene, render_camera);
 }
 
 //###### Calibration
@@ -159,57 +154,56 @@ async function estimateCameraIntrinsics(callback) {
 	canvas.width = videoTexture.image.videoWidth;
 	canvas.height = videoTexture.image.videoHeight;
 
-    // get image from video context and send it to the aruco extraction worker
+	// get image from video context and send it to the aruco extraction worker
 	ctx.drawImage(videoTexture.image, 0, 0);
-	const imageData = ctx.getImageData(0,0,videoTexture.image.videoWidth,videoTexture.image.videoHeight);
+	const imageData = ctx.getImageData(0, 0, videoTexture.image.videoWidth, videoTexture.image.videoHeight);
 
-	console.log("Use new checkerboard: "+options.cameraCalibration.use_new_board_checker);
-    // extract aruco markers
-    const aruco_points = await cv_service.extractArucoForCalib(
-        {"image" : imageData, "view_id" : options.cameraCalibration.cur_view_id, "use_new_board" : options.cameraCalibration.use_new_board_checker, "dist_to_measure": dist_to_measure});
+	// extract aruco markers
+	const aruco_points = await cv_service.extractArucoForCalib(
+		{ "image": imageData, "view_id": options.cameraCalibration.cur_view_id, "use_new_board": options.cameraCalibration.use_new_board_checker, "dist_to_measure": dist_to_measure });
 
-	let remainingImages = options.cameraCalibration.min_init_images-aruco_points.data.payload["view_id"]-1;
-	
-    if (aruco_points.data.payload["has_motion_blur"]) { // Motion blur too high. Move slowly!
+	let remainingImages = options.cameraCalibration.min_init_images - aruco_points.data.payload["view_id"] - 1;
+
+	if (aruco_points.data.payload["has_motion_blur"]) { // Motion blur too high. Move slowly!
 		camera_initialized = false;
-		
-		remainingImages = options.cameraCalibration.min_init_images-options.cameraCalibration.cur_view_id;
+
+		remainingImages = options.cameraCalibration.min_init_images - options.cameraCalibration.cur_view_id;
 		callback(remainingImages, true);
-    } else {
-        options.cameraCalibration.cur_view_id += 1;
-		const view_id = "view_"+aruco_points.data.payload["view_id"]
-		
-        // background_scene is global
-        init_scene[view_id] = {};
-        init_scene[view_id]["obj_pts"] = aruco_points.data.payload["obj_pts_js"];
-        init_scene[view_id]["img_pts"] = aruco_points.data.payload["corners_js"];
-        init_scene[view_id]["image_size"] = aruco_points.data.payload["image_size"];
+	} else {
+		options.cameraCalibration.cur_view_id += 1;
+		const view_id = "view_" + aruco_points.data.payload["view_id"]
 
-        const camera = await cv_service.estimateInitialCamera(init_scene);
-        options.cameraCalibration.fx = camera.data.payload["camera_matrix"][0][0];
-        options.cameraCalibration.fy = camera.data.payload["camera_matrix"][1][1];
-        options.cameraCalibration.cx = camera.data.payload["camera_matrix"][0][2];
-        options.cameraCalibration.cy = camera.data.payload["camera_matrix"][1][2];
-        camera_matrix = [options.cameraCalibration.fx, 0.0, options.cameraCalibration.cx, 0.0, options.cameraCalibration.fy, options.cameraCalibration.cy, 0.0, 0.0, 1.0];
-        dist_coeffs = [ 0.0, 0.0, 0.0, 0.0, 0.0];
+		// background_scene is global
+		init_scene[view_id] = {};
+		init_scene[view_id]["obj_pts"] = aruco_points.data.payload["obj_pts_js"];
+		init_scene[view_id]["img_pts"] = aruco_points.data.payload["corners_js"];
+		init_scene[view_id]["image_size"] = aruco_points.data.payload["image_size"];
 
-        options.cameraCalibration.fov = 2.0 * Math.atan(videoTexture.image.videoHeight / (2.0*options.cameraCalibration.fy)) * 180.0 / Math.PI;
+		const camera = await cv_service.estimateInitialCamera(init_scene);
+		options.cameraCalibration.fx = camera.data.payload["camera_matrix"][0][0];
+		options.cameraCalibration.fy = camera.data.payload["camera_matrix"][1][1];
+		options.cameraCalibration.cx = camera.data.payload["camera_matrix"][0][2];
+		options.cameraCalibration.cy = camera.data.payload["camera_matrix"][1][2];
+		camera_matrix = [options.cameraCalibration.fx, 0.0, options.cameraCalibration.cx, 0.0, options.cameraCalibration.fy, options.cameraCalibration.cy, 0.0, 0.0, 1.0];
+		dist_coeffs = [0.0, 0.0, 0.0, 0.0, 0.0];
 
-        if (aruco_points.data.payload["view_id"] == options.cameraCalibration.min_init_images-1) { // Camera initialized
+		options.cameraCalibration.fov = 2.0 * Math.atan(videoTexture.image.videoHeight / (2.0 * options.cameraCalibration.fy)) * 180.0 / Math.PI;
+
+		if (aruco_points.data.payload["view_id"] == options.cameraCalibration.min_init_images - 1) { // Camera initialized
 			camera_initialized = true;
-			
+
 			render_camera.fov = options.cameraCalibration.fov;
 			updateCameraChange();
 		}
-		
+
 		callback(remainingImages);
-    }
+	}
 }
 
 
 // send image data to the webworker and get camera pose
 async function estimatePoseAruco() {
-	
+
 	//just fps calculations
 	const now = performance.now();
 	while (poseTimes.length > 0 && poseTimes[0] <= now - 1000) {
@@ -220,23 +214,23 @@ async function estimatePoseAruco() {
 
 	poseEstimationRunning = true;
 
-    // get image from video context and send it to the aruco extraction worker
+	// get image from video context and send it to the aruco extraction worker
 	ctx.drawImage(videoTexture.image, 0, 0);
-	const imageData = ctx.getImageData(0,0,videoTexture.image.videoWidth,videoTexture.image.videoHeight);
+	const imageData = ctx.getImageData(0, 0, videoTexture.image.videoWidth, videoTexture.image.videoHeight);
 
-    const pose_payload = await cv_service.poseEstimation(
-        {"image" : imageData, "camera_matrix" : camera_matrix, "dist_coeffs" : dist_coeffs, "use_new_board": options.cameraCalibration.use_new_board_checker, "dist_to_measure": dist_to_measure});
-    const pose = pose_payload.data.payload;
-    const quat_xyzw = pose["quaternion_xyzw"];
-    const quaternion = new THREE.Quaternion().set(quat_xyzw[0],quat_xyzw[1],quat_xyzw[2],quat_xyzw[3]).normalize();
+	const pose_payload = await cv_service.poseEstimation(
+		{ "image": imageData, "camera_matrix": camera_matrix, "dist_coeffs": dist_coeffs, "use_new_board": options.cameraCalibration.use_new_board_checker, "dist_to_measure": dist_to_measure });
+	const pose = pose_payload.data.payload;
+	const quat_xyzw = pose["quaternion_xyzw"];
+	const quaternion = new THREE.Quaternion().set(quat_xyzw[0], quat_xyzw[1], quat_xyzw[2], quat_xyzw[3]).normalize();
 
 	// Is camera pose valid?
-    if (pose["valid"]) {
-		render_camera.position.set(pose["position"][0], pose["position"][1], pose["position"][2]);        
+	if (pose["valid"]) {
+		render_camera.position.set(pose["position"][0], pose["position"][1], pose["position"][2]);
 		render_camera.quaternion.copy(quaternion);
 
 		document.getElementById("debug").innerText = "";
-    } else {
+	} else {
 		document.getElementById("debug").innerText = "Cam pose INVALID";
 	}
 	// options.tracking.time = pose["est_time"].toFixed(2);
@@ -249,7 +243,7 @@ async function estimatePoseAruco() {
 init();
 render();
 
-window.addEventListener("beforeunload",stopCamera); // not sure if this is necessary
+window.addEventListener("beforeunload", stopCamera); // not sure if this is necessary
 
 
 
@@ -259,22 +253,22 @@ window.addEventListener("beforeunload",stopCamera); // not sure if this is neces
 const check_l = 0.015;
 
 // draw coordinate system
-const red_line = new THREE.LineBasicMaterial({color: 0xff0000, linewidth: 2});
+const red_line = new THREE.LineBasicMaterial({ color: 0xff0000, linewidth: 2 });
 const x_line = [];
-x_line.push(new THREE.Vector3(0.0,0.0,0.0))
-x_line.push(new THREE.Vector3(check_l,0.0,0.0))
+x_line.push(new THREE.Vector3(0.0, 0.0, 0.0))
+x_line.push(new THREE.Vector3(check_l, 0.0, 0.0))
 const y_line = [];
-y_line.push(new THREE.Vector3(0.0,0.0,0.0))
-y_line.push(new THREE.Vector3(0.0,check_l,0.0))
+y_line.push(new THREE.Vector3(0.0, 0.0, 0.0))
+y_line.push(new THREE.Vector3(0.0, check_l, 0.0))
 const z_line = [];
-z_line.push(new THREE.Vector3(0.0,0.0,0.0))
-z_line.push(new THREE.Vector3(0.0,0.0,check_l))
+z_line.push(new THREE.Vector3(0.0, 0.0, 0.0))
+z_line.push(new THREE.Vector3(0.0, 0.0, check_l))
 const geometry_x = new THREE.BufferGeometry().setFromPoints(x_line);
 const geometry_y = new THREE.BufferGeometry().setFromPoints(y_line);
 const geometry_z = new THREE.BufferGeometry().setFromPoints(z_line);
-const linex = new THREE.Line( geometry_x,  new THREE.LineBasicMaterial({ color: 0xff0000 }));
-const liney = new THREE.Line( geometry_y,  new THREE.LineBasicMaterial({ color: 0x00ff00 }));
-const linez = new THREE.Line( geometry_z,  new THREE.LineBasicMaterial({ color: 0x0000ff }));
+const linex = new THREE.Line(geometry_x, new THREE.LineBasicMaterial({ color: 0xff0000 }));
+const liney = new THREE.Line(geometry_y, new THREE.LineBasicMaterial({ color: 0x00ff00 }));
+const linez = new THREE.Line(geometry_z, new THREE.LineBasicMaterial({ color: 0x0000ff }));
 scene.add(linex);
 scene.add(liney);
 scene.add(linez);
